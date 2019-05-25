8b826173643566a8b393a0f5e7f22a6e296dc7a6
jnbal <jnball10@outlook.com>
2019-05-25 16:51:37 -0700

    character selection page added

--- app/src/main/java/com/example/co2/CharacterSelection.java
+++ app/src/main/java/com/example/co2/CharacterSelection.java
@@ -2,12 +2,32 @@ package com.example.co2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CharacterSelection extends AppCompatActivity {
+
+    ListView listView;
+    List characters = new ArrayList();
+    ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
+
+        listView = (ListView)findViewById(R.id.list_view);
+
+        characters.add("Jonathan");
+        characters.add("Carlos");
+        characters.add("Garfield");
+
+        adapter = new ArrayAdapter(CharacterSelection.this, android.R.layout.simple_list_item_1, characters);
+        listView.setAdapter(adapter);
     }
+
 }
--- app/src/main/java/com/example/co2/HomePage.java
+++ app/src/main/java/com/example/co2/HomePage.java
@@ -1,13 +1,30 @@
 package com.example.co2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.co2.R.id.characters;
 
 public class HomePage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Button character = (Button) findViewById(characters);
+        character.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CharacterSelection.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }
